96b00fa0442dde4e58d5e78120cf9232528597da
Clamentos <e.gattomonticone@gmail.com>
2025-11-06 22:09:51 +0100

    added jvm heap cap

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
-nohup java -jar *.jar -Dspring.profiles.active=prod --server.ssl.key-store-password=$0 --app.metrics.apiKey=$1 &
+nohup java -jar *.jar -Dspring.profiles.active=prod --server.ssl.key-store-password=$0 --app.metrics.apiKey=$1 -Xmx2G &
 echo $! > ./current_pid.txt
