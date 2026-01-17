e475b3b55445c961b026fd7f032d7f0712c1aaf1
Enrico Gatto Monticone <e.gattomonticone@gmail.com>
2026-01-17 21:55:30 +0100

    fix on start script (hopefully)

--- resources/scripts/start.sh
+++ resources/scripts/start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-setsid java -jar $TARGET_DIR/gattoslab*.jar --spring.profiles.active=prod --server.ssl.key-store-password=$KEYSTORE_PASSWORD --app.session.admin.apiKey=$API_KEY --app.database.connectionString=$DB_STRING -Xmx1024m < /dev/null &
+setsid java -jar ../gattoslab*.jar --spring.profiles.active=prod --server.ssl.key-store-password=$KEYSTORE_PASSWORD --app.session.admin.apiKey=$API_KEY --app.database.connectionString=$DB_STRING -Xmx1024m < /dev/null &
