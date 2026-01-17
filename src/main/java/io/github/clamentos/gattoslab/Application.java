bff76a0a7521a7c632288d372253c668eaa66bd7
Enrico Gatto Monticone <e.gattomonticone@gmail.com>
2026-01-17 18:06:07 +0100

    Regenerated site files Diverted stdout on a file Updated yaml files

--- src/main/java/io/github/clamentos/gattoslab/Application.java
+++ src/main/java/io/github/clamentos/gattoslab/Application.java
@@ -1,6 +1,11 @@
 package io.github.clamentos.gattoslab;
 
 ///
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintStream;
+
+///.
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,7 +16,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	///
-	public static void main(String[] args) {
+	public static void main(final String[] args) throws IOException {
+
+		final PrintStream consoleOut = new PrintStream("./console_out.log");
+
+		System.setOut(consoleOut);
+		System.setErr(consoleOut);
+
+		try(final FileWriter pidFile = new FileWriter("./pid.txt")) {
+
+			pidFile.write(Long.toString(ProcessHandle.current().pid()));
+		}
 
 		SpringApplication.run(Application.class, args);
 	}
