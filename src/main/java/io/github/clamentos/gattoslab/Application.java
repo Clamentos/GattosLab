809fd435e25e723eae518fe369ace8caf6f85e43
Clamentos <enrico.gattomonticone@protonmail.com>
2026-05-05 18:39:18 +0200

    added temp debug log

--- src/main/java/io/github/clamentos/gattoslab/Application.java
+++ src/main/java/io/github/clamentos/gattoslab/Application.java
@@ -245,6 +245,8 @@ public class Application {
 
         try(InputStream keyStream = Application.class.getClassLoader().getResourceAsStream(name)) {
 
+            log.info("DEBUG keystore.p12 exists {}", keyStream != null);
+
             final KeyStore loadedKeystore = KeyStore.getInstance("JKS");
             loadedKeystore.load(keyStream, password.toCharArray());
 
